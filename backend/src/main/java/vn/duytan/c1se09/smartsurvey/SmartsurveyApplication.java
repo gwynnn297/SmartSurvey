08f635972a12b087f0932c7419f8019fbcb18c2d
gwynnn297 <phuchoang29072000@gmail.com>
2025-08-29 20:34:24 +0700

    build Smartservey project backend

---
+++ backend/src/main/java/vn/duytan/c1se09/smartsurvey/SmartsurveyApplication.java
@@ -0,0 +1,13 @@
+package vn.duytan.c1se09.smartsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/vn/duytan/c1se09/smartsurvey/SmartsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.duytan.c1se09.smartsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
